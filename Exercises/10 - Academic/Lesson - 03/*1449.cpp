10d4a8f622b61dbe0987e238dae492d9b950d9dd
Digao-Onofri <goncalves.onofri@gmail.com>
2025-09-11 18:38:36 -0300

    fixing errors in files of Lesson 3

--- Exercises/10 - Academic/Lesson - 03/*1449.cpp
+++ Exercises/10 - Academic/Lesson - 03/*1449.cpp
@@ -8,8 +8,8 @@ using namespace std;
 
 int main(){
     map<string, string> lyrics;
-    int instances, words, lines;
-    string song, japanese, portuguese;
+    int instances, words, lines, i, spaces;
+    char song[81], japanese[81], portuguese[81], word[81];
 
     cin >> instances;
 
@@ -17,17 +17,30 @@ int main(){
         cin >> words >> lines;
 
         while(words--){
-            getline(cin, japanese);
-            getline(cin,  portuguese);
+            scanf(" %[^\n]", japanese);
+            scanf(" %[^\n]", portuguese);
             lyrics.insert({japanese, portuguese});
-            cout << lyrics[japanese] << 'debug\n';
         }
 
-        
-
         while(lines){
-            getline(cin, song);
-            cout << lyrics[song] << '\n';
+            scanf(" %[^\n]", song);
+            
+            spaces = 0;
+            for(i = 0; i < strlen(song); i++){
+                if(song[i] == ' '){
+                    spaces++;
+                }
+            }
+
+            while(spaces){
+                for(i = 0; i < strlen(song); i++){
+                    if(song[i] == ' '){
+                        j = 0;
+                        spaces--;
+                        cout << lyrics[word];
+                    }
+                }
+            }
         }
     }
 
--- Exercises/10 - Academic/Lesson - 03/1039.c
+++ Exercises/10 - Academic/Lesson - 03/1039.c
@@ -8,18 +8,17 @@
 
 int main(){
     int R1, X1, Y1, R2, X2, Y2;
-    
+    int distance;
 
     while(scanf("%d %d %d %d %d %d", &R1, &X1, &Y1, &R2, &X2, &Y2) != EOF){
-        X1 = fabs(X1);
-        Y1 = fabs(Y1);
-        X2 = fabs(X2);
         
-        if(fabs(X1 - X2) > R1 || fabs(Y1 - Y2) > R1 || R2 >= R1){
-            printf("MORTO\n");
+        distance = sqrt((X1 - X2) * (X1 - X2) + (Y1 - Y2) * (Y1 - Y2));
+
+        if(distance + R2 < R1){
+            printf("RICO\n");
         }
         else{
-            printf("RICO\n");
+            printf("MORTO\n");
         }
 
     }
