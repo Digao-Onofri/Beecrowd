57086eccf181ac93c903d9aac2917c7acdf38f26
Digao-Onofri <your_email@example.com>
2025-10-30 15:16:36 -0300

    final solution to 1128

--- Exercises/10 - Academic/Lesson - 08/1128.cpp
+++ Exercises/10 - Academic/Lesson - 08/1128.cpp
@@ -6,30 +6,27 @@
 #include <bits/stdc++.h> 
 using namespace std;
 
-void dfs_rec(vector<int> &connected, int *visited, int graph[][2010], int n, int u){
+void dfs_rec(int *visited, int graph[][2010], int n, int u){
     int v;
     visited[u] = 1;
 
-    connected.push_back(u);
-
     for(v = 1; v <= n; v++){
         if(graph[u][v] > 0 && visited[v] == 0){
-            dfs_rec(connected, visited, graph, n, v);
+            dfs_rec(visited, graph, n, v);
         }
     }
 }
 
 int graph[2010][2010];
+int reversegraph[2010][2010];
 
 int main(){
     int visited[2010];
-    vector<int> connected;
-    vector<int>::iterator it;
-    int vertices, edges;
+    int vertices, edges, i;
     int verta, vertb, way;
+    bool ok; 
 
     while(cin >> vertices >> edges){
-        connected.clear();
 
         if(vertices == 0 && edges == 0){
             break;
@@ -37,39 +34,45 @@ int main(){
 
         memset(visited, 0, sizeof(visited));
         memset(graph, 0, sizeof(graph));
+        memset(reversegraph, 0, sizeof(reversegraph));
 
         while(edges--){
             scanf("%d %d %d", &verta, &vertb, &way);
             if(way == 1){
                 graph[verta][vertb] = 1;
+                reversegraph[vertb][verta] = 1;
             }
             else{
                 graph[verta][vertb] = 1;
                 graph[vertb][verta] = 1;
+                reversegraph[vertb][verta] = 1;
+                reversegraph[verta][vertb] = 1;
             }
         }
 
-        dfs_rec(connected, visited, graph, vertices, 1); 
-        if(connected.size() == vertices){
-            cout << 1 << endl;
-        }
-        else{
-            cout << 0 << endl;
+        ok = true;
+        dfs_rec(visited, graph, vertices, 1); 
+        for(i = 1; i <= vertices; i++){
+            if(!visited[i]){
+                ok = false;
+                break;
+            }
         }
 
-        //for(i = 1; i <= vertices; i++){
-        //     dfs_rec(connected, visited, graph, vertices, i); 
-        //     if(connected.size() != vertices){
-        //         cout << 0 << endl;
-        //         printed = true;
-        //         break;
-        //     }
-        //     connected.clear();
-        // }
+        if(ok){
+            memset(visited, 0, sizeof(visited));
+            dfs_rec(visited, reversegraph, vertices, 1);
+            for (i = 1; i <= vertices; i++) {
+                if (!visited[i]) {
+                    ok = false;
+                    break;
+                }
+            }
+
+        }
+
+        cout << (ok ? 1 : 0) << endl;
         
-        // if(!printed){
-        //     cout << 1 << endl;
-        // }
     }
 
     return 0;
