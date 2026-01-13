7c13e7871e48416aabd8e209b26858e22382b317
Digao-Onofri <goncalves.onofri@gmail.com>
2026-01-13 15:02:15 -0300

    [add] including maratona IX attempt

---
+++ Codeforces/Groups/Maratona Mineira/IX/l.cpp
@@ -0,0 +1,13 @@
+/*
+    Name: Se liga na cifra
+    Link: https://codeforces.com/group/YgJmumGtHD/contest/103794/problem/L
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+
+    return 0;
+}
